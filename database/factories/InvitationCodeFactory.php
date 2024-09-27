f7979714031b1d85bfee512e61c9cc0decdd72c3
Hafez Madi <hafezbenabdillah.madi@gmail.com>
2024-09-27 15:09:25 +0200

    small fixes

--- database/factories/InvitationCodeFactory.php
+++ database/factories/InvitationCodeFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\InvitationCode>
@@ -18,7 +19,7 @@ class InvitationCodeFactory extends Factory
     public function definition(): array
     {
         return [
-            'code' => fake()->bothify('????-####-????'),
+            'code' => Str::upper(fake()->bothify('????-####-????')),
             'consumed_at' => null,
             'owner_id' => User::factory(),
             'consumer_id' => null
